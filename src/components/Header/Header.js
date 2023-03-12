ce2b1531c0c5e587d6f853f424d490a3df20403c
Ben <stealthton@gmail.com>
2023-03-12 16:09:10 +0000

    reeduced enlarge % on cards and bottom nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,16 +11,16 @@ function Header() {
 
 <ul className="nav justify-content-center">
   <li className="nav-item">
-    <a className="nav-link" href="#/">Home</a>
+    <a className="nav-link header-link" href="#/">Home</a>
   </li>
   <li className="nav-item">
-    <a className="nav-link" href="#/About">About</a>
+    <a className="nav-link header-link" href="#/About">About</a>
   </li>
   <li className="nav-item">
-    <a className="nav-link" href="#/Contact">Contact</a>
+    <a className="nav-link header-link" href="#/Contact">Contact</a>
   </li>
   <li className="nav-item">
-    <a className="nav-link" href="#/ProjectGallery">Project Gallery</a>
+    <a className="nav-link header-link" href="#/ProjectGallery">Project Gallery</a>
   </li>
 </ul>
 <hr></hr>
